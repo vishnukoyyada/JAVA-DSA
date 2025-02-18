a21d9e83ddf3535911a8687379b2232cc51568ab
vishnuvardhan <vishnukoyyada03@gmail.com>
2025-02-18 22:24:25 +0530

    Added linkedList

--- LinkedList/Implementation.java
+++ LinkedList/Implementation.java
@@ -25,6 +25,30 @@ class LL {
             temp.next = newnode;
         }
     }
+    public void insertFirst(int val){
+        Node newnode = new Node(val);
+        newnode.next = head;
+        head= newnode;
+    }
+    public void Delete(int val){
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        // If the head node itself holds the key to be deleted
+        if (head.data == val) {
+            head = head.next;
+            return;
+        }
+        Node prev = null;
+        Node temp = head;
+        while(temp != null && temp.data != val){
+            prev= temp;
+            temp = temp.next;
+        }
+        prev.next = temp.next;
+    }
 
     public void display() {
         Node temp = head;
@@ -43,6 +67,9 @@ public class Implementation {
         link.insert(3);
         link.insert(4);
         link.insert(5);
+        link.insertFirst(0);
+        link.insertFirst(-1);
+        link.Delete(4);
 
         link.display();
     }
